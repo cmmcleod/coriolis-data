cca457f08ca903c88dd56411d4cad91c9c1a98c0
Colin McLeod <colin@roaminsight.com>
2016-02-12 14:24:01 -0800

    add shield recovery rates

--- generate_index.js
+++ generate_index.js
@@ -2,10 +2,14 @@ var fs = require('fs');
 var UglifyJS = require('uglify-js');
 var Ships = require('./Ships');
 var Modules = require('./Modules');
-
+var exportString = JSON.stringify({ Ships: Ships, Modules: Modules }, null, '\t');
 var ast = UglifyJS.parse(['module.exports = ', JSON.stringify({ Ships: Ships, Modules: Modules }), ';'].join(''));
 var code = ast.print_to_string({beautify: true, indent_level: 2});
 
 fs.open('./index.js', 'w', function() {
     fs.writeFile('./index.js', code, function(err) {});
+});
+
+fs.open('./index.json', 'w', function() {
+    fs.writeFile('./index.json', code, function(err) {});
 });
